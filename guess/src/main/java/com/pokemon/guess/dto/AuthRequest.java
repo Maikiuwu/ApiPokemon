c6f2029d5e49fe21f8bdabe1472064282a47c13c
mega <sfssanchez@gmail.com>
2025-06-09 19:12:29 -0500

    cambio de base de datos a supabase

--- guess/src/main/java/com/pokemon/guess/dto/AuthRequest.java
+++ guess/src/main/java/com/pokemon/guess/dto/AuthRequest.java
@@ -10,5 +10,23 @@ public class AuthRequest {
     @NotBlank
     private String password;
 
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    
     // Getters y setters
 }
